d3b9b209f8b47051feffcf5191d8a3be7fef5cd8
jclezamap <81341253+jclezamap@users.noreply.github.com>
2022-11-21 22:42:22 -0500

    Update __init__.py

--- calificar/__init__.py
+++ calificar/__init__.py
@@ -1,28 +1,34 @@
 """calificar - An opinionated, minimal cookiecutter template for Python packages"""
 
-__version__ = '1.0'
+__version__ = '1.1'
 __author__ = 'Juan Carlos Lezama <jclezamap@gmail.com>'
 __all__ = []
-
 
 def Calificar( Integrante , Grupo , Respuestas, Taller="", Curso=None):
     import requests
     Respuestas2=""
-    if Curso is None:
-        Curso='0'
-    if type(Respuestas.tolist()) == list:
-        Respuestas2=';'.join(map(str, Respuestas))
-    datos = {'Integrantes': Integrante, 'Grupo': Grupo,'Curso':Curso,'Respuestas':Respuestas2, 'python':1}
-    resp = requests.post('https://economiafinanciera.com.co/?app=excel&modulo=taller&accion=respuestas&Actividad='+Taller, data=datos)
-    return "Validación de Puntos:"+resp.text.split("|")[0]+"\r\nNota:"+resp.text.split("|")[1]+"\r\nMensaje:"+resp.text.split("|")[2][1:]    
+    try:
+        if Curso is None:
+            Curso='0'            
+        if type(Respuestas.tolist()) == list:
+            Respuestas2=';'.join(map(str, Respuestas))
+        datos = {'Integrantes': Integrante, 'Grupo': Grupo,'Curso':Curso,'Respuestas':Respuestas2, 'python':1}
+        resp = requests.post('https://economiafinanciera.com.co/in?app=excel&modulo=taller&accion=respuestas&Actividad='+Taller, data=datos)
+        return "Validación de Puntos:"+resp.text.split("|")[0]+"\r\nNota:"+resp.text.split("|")[1]+"\r\nMensaje:"+resp.text.split("|")[2][1:]    
+    except:
+        return resp.text
 
 def ValidacionPuntos( Integrante , Grupo , Respuestas, Taller="", Curso=None):
     import requests
     Respuestas2=""
-    if Curso is None:
-        Curso='0'
-    if type(Respuestas.tolist()) == list:
-        Respuestas2=';'.join(map(str, Respuestas))
-    datos = {'Integrantes': Integrante, 'Grupo': Grupo,'Curso':Curso,'Respuestas':Respuestas2, 'python':1}
-    resp = requests.post('https://economiafinanciera.com.co/?app=excel&modulo=taller&accion=respuestas&Actividad='+Taller, data=datos)
-    return resp.text.split("|")[0].split(";")
+    try:
+        if Curso is None:
+            Curso='0'
+        if type(Respuestas.tolist()) == list:
+            Respuestas2=';'.join(map(str, Respuestas))
+        datos = {'Integrantes': Integrante, 'Grupo': Grupo,'Curso':Curso,'Respuestas':Respuestas2, 'python':1}
+        resp = requests.post('https://www.economiafinanciera.com.co/indexdes.php?app=excel&modulo=taller&accion=respuestas&Actividad='+Taller, data=datos)
+        return resp.text.split("|")[0].split(";")
+    except:
+        return resp.text
+
